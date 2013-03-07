2a1266831dbefdbb3617741e94688e87306bbd86
Kiran <gudapudi.kiran@gmail.com>
2013-03-07 20:43:20 +0530

    Initial version

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        public ActionResult Index()
+        {
+            var genre = storeDB.Genres.ToList();
+
+            return View(genre);
+        }
+
+        //GET: /Browse/
+
+        public ActionResult Browse( string genre)
+        {
+            var gerneModel = storeDB.Genres.Include("Albums").Single(g => g.Name == genre);
+            return View(gerneModel);
+        
+        }
+        //GET: /Details/
+
+        public ActionResult Details(int id )
+        {
+            var album = storeDB.Albums.Find(id);
+            return View(album);
+
+        }
+    }
+}
